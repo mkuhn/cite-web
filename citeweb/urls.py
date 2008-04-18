a60bfe7b826b09ecc98f8f09e9e72975998a02f1
Michael Kuhn <mkuhn@altadena.local>
2008-04-18 19:05:18 +0200

    create project with django default settings

---
+++ citeweb/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^citeweb/', include('citeweb.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
